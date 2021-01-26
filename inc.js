9b25af946b783740616770a08d7c0af697a4820c
wzc-Seer <semfuchs211@gmail.com>
2021-01-26 10:13:56 +0100

    Add notifier

--- inc.js
+++ inc.js
@@ -1,3 +1,7 @@
 const client = require("./client");
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+ 
+updateNotifier({pkg}).notify();
 
 module.exports = client;
